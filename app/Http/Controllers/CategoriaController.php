70f7304804b77186deef2341390d6509d70c8f3e
wapmgit <33326378+wapmgit@users.noreply.github.com>
2025-10-30 16:27:53 -0430

    act categoria act util

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -83,56 +83,56 @@ class CategoriaController extends Controller
 			$tasa=$request->get('tasa');
             $categoria=$request->get('categoria');
 			if ($modo == 1){
-			$articulos=DB::table('articulos as a')
-            -> select ('a.idarticulo','a.utilidad','a.util2','iva','a.costo','a.precio1','a.precio2')
-            ->where ('a.idcategoria','=',$categoria)
-			->get();
-			 $cont = 0;
-			
-			 if($empresa->calc_util==1){
-			while($cont < count($articulos)){ 
-			$articulo=Articulos::findOrFail($articulos[$cont]->idarticulo); 			
-		    $nprecio=$articulo->precio1+(($tasa/100)*$articulos[$cont]->precio1);
-			$pt=($articulos[$cont]->costo + (($articulos[$cont]->iva/100)*$articulos[$cont]->costo));  
-			  $nutil=((($nprecio/$pt)*100)-100);
-			  if($request->get('precio')==0){
-			  $articulo->precio1=$nprecio;
-			  $articulo->utilidad=$nutil;
-			  $articulo->precio2=$nprecio;
-			  $articulo->util2=$nutil; }
-			  if($request->get('precio')==1){
-			  $articulo->precio1=$nprecio;
-			  $articulo->utilidad=$nutil; }
-			   if($request->get('precio')==2){
-			  $articulo->precio2=$nprecio;
-			  $articulo->util2=$nutil; }
-			  $articulo->update();
-            $cont=$cont+1;
-            }     	
-			 }else{
-			while($cont < count($articulos)){ 
-			$articulo=Articulos::findOrFail($articulos[$cont]->idarticulo); 			
-				$nprecio=($articulo->precio1/((100-$tasa)/100)); 
-				$nutil=($nprecio/(($articulos[$cont]->iva/100)+1)/$articulos[$cont]->costo);
-				$nutil= ($nutil-1)*100;
-				 $nprecio2=($articulo->precio2/((100-$tasa)/100)); 
-				$nutil2=($nprecio2/(($articulos[$cont]->iva/100)+1)/$articulos[$cont]->costo);
-				$nutil2= ($nutil2-1)*100;
-			  if($request->get('precio')==0){
-			  $articulo->precio1=$nprecio;
-			  $articulo->utilidad=$nutil;
-			  $articulo->precio2=$nprecio2;
-			  $articulo->util2=$nutil2; }
-			  if($request->get('precio')==1){
-			  $articulo->precio1=$nprecio;
-			  $articulo->utilidad=$nutil; }
-			   if($request->get('precio')==2){
-			  $articulo->precio2=$nprecio2;
-			  $articulo->util2=$nutil2; }
-			  $articulo->update();
-            $cont=$cont+1;
-            }   
-			 }
+				$articulos=DB::table('articulos as a')
+				-> select ('a.idarticulo','a.utilidad','a.util2','iva','a.costo','a.precio1','a.precio2')
+				->where ('a.idcategoria','=',$categoria)
+				->get();
+				 $cont = 0;
+				
+					 if($empresa->calc_util==1){
+						while($cont < count($articulos)){ 
+						$articulo=Articulos::findOrFail($articulos[$cont]->idarticulo); 			
+						$nprecio=$articulo->precio1+(($tasa/100)*$articulos[$cont]->precio1);
+						$pt=($articulos[$cont]->costo + (($articulos[$cont]->iva/100)*$articulos[$cont]->costo));  
+						  $nutil=((($nprecio/$pt)*100)-100);
+						  if($request->get('precio')==0){
+						  $articulo->precio1=$nprecio;
+						  $articulo->utilidad=$nutil;
+						  $articulo->precio2=$nprecio;
+						  $articulo->util2=$nutil; }
+						  if($request->get('precio')==1){
+						  $articulo->precio1=$nprecio;
+						  $articulo->utilidad=$nutil; }
+						   if($request->get('precio')==2){
+						  $articulo->precio2=$nprecio;
+						  $articulo->util2=$nutil; }
+						  $articulo->update();
+						$cont=$cont+1;
+						}     	
+					 }else{
+						while($cont < count($articulos)){ 
+						$articulo=Articulos::findOrFail($articulos[$cont]->idarticulo); 			
+							$nprecio=($articulo->precio1/((100-$tasa)/100)); 
+							$nutil=($nprecio/(($articulos[$cont]->iva/100)+1)/$articulos[$cont]->costo);
+							$nutil= ($nutil-1)*100;
+							 $nprecio2=($articulo->precio2/((100-$tasa)/100)); 
+							$nutil2=($nprecio2/(($articulos[$cont]->iva/100)+1)/$articulos[$cont]->costo);
+							$nutil2= ($nutil2-1)*100;
+						  if($request->get('precio')==0){
+						  $articulo->precio1=$nprecio;
+						  $articulo->utilidad=$nutil;
+						  $articulo->precio2=$nprecio2;
+						  $articulo->util2=$nutil2; }
+						  if($request->get('precio')==1){
+						  $articulo->precio1=$nprecio;
+						  $articulo->utilidad=$nutil; }
+						   if($request->get('precio')==2){
+						  $articulo->precio2=$nprecio2;
+						  $articulo->util2=$nutil2; }
+						  $articulo->update();
+						$cont=$cont+1;
+						}   
+						 }
 			}
 			if ($modo == 2){
 			$articulos=DB::table('articulos as a')
@@ -170,7 +170,7 @@ class CategoriaController extends Controller
 			$articulo->utilidad=$tasa;
 			$impuesto=$articulo->iva;
 			$costo=$articulo->costo;
-			$np=($costo / ((100-$tasa)/100))+(($impuesto/100)*$costo);
+			$np=($costo / ((100-$tasa)/100))+(($impuesto/100)*($costo / ((100-$tasa)/100)));
 			 if($request->get('precio')==0){
 				$articulo->precio1=$np;
 				$articulo->utilidad=$tasa;
