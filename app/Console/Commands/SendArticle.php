855ac4c8836b32b204aafaaa62f3e7dd77ec7f68
wapmgit <33326378+wapmgit@users.noreply.github.com>
2026-02-13 11:51:21 -0430

    act apis

--- app/Console/Commands/SendArticle.php
+++ app/Console/Commands/SendArticle.php
@@ -41,24 +41,10 @@ class SendArticle extends Command
     public function handle()
     {
 		$empresa=DB::table('empresa')->first();
-		$article = Articulo::selectRaw("
-        codweb as barcode,
-        SUBSTRING_INDEX(nombre, '*', 1) as name,
-        ROUND(stock, 2) as stock,
-        iva,
-        IF(cntxund > 1, ROUND((precio1 / cntxund), 2), ROUND(precio1, 2)) as price,
-        unidad as unit,
-        ROUND(cntxund, 0) as cntxund,
-        ROUND(cntgrupo, 0) as cntgrupo,
-        ROUND(fraccion, 2) as fraccion,
-        peso as weight,
-        codigo as codesv,
-        imagen as img
-    ")
-    ->where('stock', '>', 0)
-    ->whereNotNull('codweb')
-    ->orderBy('name', 'asc')
-    ->get();
+		$article = DB::table('articulos')->join('categoria as cat','cat.idcategoria','=','articulos.idcategoria')
+			->select ('articulos.idarticulo','articulos.codigo','articulos.nombre','articulos.costo','articulos.precio1','articulos.precio2','articulos.precio3','articulos.stock','articulos.imagen','articulos.fraccion','articulos.cntxund','articulos.cntgrupo')
+			->where('articulos.estado','=',"Activo")
+			->get(); 
 			$articlejs=json_encode($article);
 
             $response = Http::post('http://creciven.com/api/recibir-articulos', [
--- app/Console/Commands/SendtoMercarapi.php
+++ app/Console/Commands/SendtoMercarapi.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use Exception;
 use App\Models\Empresa;
+use App\Models\Articulo;
 use DB;
 use Carbon\Carbon;
 
@@ -44,12 +45,29 @@ class SendtoMercarapi extends Command
     {
 			$emp=Empresa::findOrFail(1);
 			$empresa=$emp->uuid;
-			  $products=DB::table('articulos as art')
+		/*	  $products=DB::table('articulos as art')
 				->select('art.codweb as barcode','art.nombre as name','art.stock','art.iva','art.precio1 as price','art.unidad as unit','art.cntxund','art.cntgrupo','art.fraccion','art.peso as weight','art.codigo as codesv','art.imagen as img')
 				->where('art.stock','>=','0')
 				->whereNotNull('art.codweb')
-				->get()->toArray();
-				
+				->get()->toArray(); */
+		$products = Articulo::selectRaw("
+        codweb as barcode,
+        SUBSTRING_INDEX(nombre, '*', 1) as name,
+        ROUND(stock, 2) as stock,
+        iva,
+        IF(cntxund > 1, ROUND((precio1 / cntxund), 2), ROUND(precio1, 2)) as price,
+        unidad as unit,
+        ROUND(cntxund, 0) as cntxund,
+        ROUND(cntgrupo, 0) as cntgrupo,
+        ROUND(fraccion, 2) as fraccion,
+        peso as weight,
+        codigo as codesv,
+        imagen as img
+    ")
+    ->where('stock', '>', 0)
+    ->whereNotNull('codweb')
+    ->orderBy('name', 'asc')
+    ->get()->toArray();
           $response = Http::post('http://mercarapid.nks-sistemas.net/api/recibir-inventario', [
                 'store' => $empresa,
                 'productos' => ["data" => $products]
