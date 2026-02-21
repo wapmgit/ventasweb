6f04c3e1ade66eedd908246154d813a9046090c1
wapmgit <33326378+wapmgit@users.noreply.github.com>
2026-02-21 15:42:15 -0430

    delete opcion registrar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Registrarse</a>
+                                    <a class="nav-link" href="">Registrarse</a>
                                 </li>
                             @endif
                         @else
