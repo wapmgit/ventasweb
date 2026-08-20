64e4be61c197c4b1e59996cfd9876d1bb93e40e8
wapmgit <33326378+wapmgit@users.noreply.github.com>
2026-08-19 21:49:28 -0400

    export excel

--- resources/views/bancos/banco/detalle.blade.php
+++ resources/views/bancos/banco/detalle.blade.php
@@ -69,12 +69,39 @@ switch ($datmoneda->tipo){
           <th>Usuario</th>
       </TFOOT>
 			</table>
-
+   <table id="databanco" style="display:none">
+        <thead >
+        <th >Mov</th>
+        <th >Fecha</th>
+		<th>Numero</th>
+        <th>Clasificador</th>
+		<th>Concepto</th>
+		<th >Beneficiario</th>
+        <th>Tipo</th>
+        <th>Monto</th>
+        <th>Usuario</th>
+		</thead>
+<?php $tcompras=0; ?>
+               @foreach ($movimiento as $q)
+               <?php  $tcompras=($tcompras+$q->monto);?>
+				<tr> 
+	       <td>{{ $q->id_mov}}</td> 
+          <td><?php  echo $fecha=date_format(date_create($q->fecha_mov),'d-m-Y h:i:s');?></td>
+          <td> {{ $q->numero}}</td>
+          <td>{{ $q->descrip}}</td>
+		  <td>{{ $q->concepto}}</td>
+		  <td>{{ $q->identificacion}}</td>
+          <td>{{ $q->tipo_mov}}</td>
+          <td><?php echo number_format(($q->monto),'2','.',','); ?></td>	
+          <td>{{  $q->user}}</td>
+				</tr> @endforeach    
+			</table>
   </div>
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" align="center">
  
 	<a href="{{route('showbanco',['id'=>$banco->idbanco])}}"  id="regresar"><button  class="btn btn-danger btn-sm btn-pull-left" >Regresar</button></a> 
-        <a ><button class="btn btn-primary btn-sm   pull-right" id="imprimir">Imprimir</button></a>           
+<button onclick="htmlExcel('databanco', 'Mov_Bancos')" class="btn btn-warning btn-sm">Exportar XLS</button>       
+	   <a ><button class="btn btn-primary btn-sm   pull-right" id="imprimir">Imprimir</button></a>           
 	</div>
  
 
@@ -91,7 +118,36 @@ $(document).ready(function(){
     });
 
 });
+	function htmlExcel(idTabla, nombreArchivo = '') {
+	  let linkDescarga;
+	  let tipoDatos = 'application/vnd.ms-excel';
+	  let tablaDatos = document.getElementById(idTabla);
+	  let tablaHTML = tablaDatos.outerHTML.replace(/ /g, '%20');
 
+	  // Nombre del archivo
+	  nombreArchivo = nombreArchivo ? nombreArchivo + '.xls' : 'Mov_Bancos.xls';
+
+	  // Crear el link de descarga
+	  linkDescarga = document.createElement("a");
+
+	  document.body.appendChild(linkDescarga);
+
+	  if (navigator.msSaveOrOpenBlob) {
+		let blob = new Blob(['\ufeff', tablaHTML], {
+		  type: tipoDatos
+		});
+		navigator.msSaveOrOpenBlob(blob, nombreArchivo);
+	  } else {
+		// Crear el link al archivo
+		linkDescarga.href = 'data:' + tipoDatos + ', ' + tablaHTML;
+
+		// Setear el nombre de archivo
+		linkDescarga.download = nombreArchivo;
+
+		//Ejecutar la función
+		linkDescarga.click();
+	  }
+	}
 </script>
 
 @endpush
