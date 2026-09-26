d33ace04ce8f4b9757f907bcecaf83bcde9f7264
wapmgit <33326378+wapmgit@users.noreply.github.com>
2026-09-25 22:15:40 -0400

    act pvip

--- app/Console/Commands/SendArticle.php
+++ app/Console/Commands/SendArticle.php
@@ -43,7 +43,7 @@ class SendArticle extends Command
     {
 		$empresa=DB::table('empresa')->first();
 		$article = DB::table('articulos')->join('categoria as cat','cat.idcategoria','=','articulos.idcategoria')
-			->select ('cat.idcategoria','articulos.idarticulo','articulos.codigo','articulos.nombre','articulos.costo','articulos.precio1','articulos.precio2','articulos.precio3','articulos.stock','articulos.peso','articulos.imagen','articulos.fraccion','articulos.cntxund','articulos.cntgrupo')
+			->select ('cat.idcategoria','articulos.idarticulo','articulos.codigo','articulos.nombre','articulos.costo','articulos.precio1','articulos.precio2','articulos.precio3','articulos.pvip','articulos.stock','articulos.peso','articulos.imagen','articulos.fraccion','articulos.cntxund','articulos.cntgrupo')
 			->where('articulos.estado','=',"Activo")
 			->get(); 
 			$articlejs=json_encode($article);
